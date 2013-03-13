61d5d28f9b5dc1c5591de84e1efb3e98ba280b24
Daniel Büchele <daniel@buechele.cc>
2013-03-13 15:50:13 +0100

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Instawall
+
+## A Twitterwall for Instagram
+ 
+Instawall is a JavaScript-client for Instagram. Enter a hashtag and the latest photo for the tag is shown fullscreen. The script automatically updates the display, when a newer photo is available. Instawall is intended for use on status boards, public displays, etc. 
---
+++ js/domscript.js
@@ -0,0 +1,73 @@
+var api = "https://api.instagram.com/v1/tags/#hashtag/media/recent?access_token=280712.f59def8.a33f9d700568474283fa7a22020ce1f7&callback=?";
+var hashtag = "";
+var latest = "";
+var reloadTime = 3000;
+var timer;
+
+
+	
+var needsReload = function () {
+	$("#search input").focus();
+	url = api.replace("#hashtag",hashtag);
+	$.getJSON(url, function(data) {
+
+		if (data.data[0].images.standard_resolution.url!=latest) {
+			console.log("reload");
+			showImage(data.data[0].images.standard_resolution.url);
+
+		} else {
+			console.log("no update needed");	
+		}
+		
+		timer = setTimeout(needsReload,reloadTime);
+
+	});
+}
+	
+	
+$(function(){	
+	$("#search input").focus();
+	
+	
+	$(document).keypress(function(e) {
+		$("#search input").focus();
+		$("#search").fadeIn();
+	});
+	
+
+})
+
+function setHashtag() {
+	hashtag = $("#search input").val();
+	clearTimeout(timer);
+	
+	$("#search").fadeOut(100, function () {
+		$("#search input").val("");
+	});
+	
+	url = api.replace("#hashtag",hashtag);
+	$.getJSON(url, function(data) {
+		console.log("load");
+		showImage(data.data[0].images.standard_resolution.url);
+		needsReload();
+	});
+	
+	return false;
+}
+
+function showImage(url) {
+
+	console.log("showImage")
+	$.loadImages(url,function () {
+		latest = url;
+		$(".passive").css("background-image","url("+url+")");
+		$(".passive").fadeIn(500,function () {
+			$(".pic:not(.passive)").fadeOut(500,function () {
+				$(".passive").removeClass("passive");
+				$(this).addClass("passive");
+			});
+		});
+		
+
+	});
+}
